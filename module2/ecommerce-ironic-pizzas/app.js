9e7232c89956957f9e9d96c2de7be7db746c3da6
Matthew Schröder <matthewschroeder94@gmail.com>
2022-09-30 15:53:36 +0200

    add GET functionality

--- module2/ecommerce-ironic-pizzas/app.js
+++ module2/ecommerce-ironic-pizzas/app.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const hbs = require("hbs");
+const mongoose = require('mongoose');
+
+const Pizza = require("./models/Pizza.model")
+
 
 const app = express();
 
@@ -12,11 +16,31 @@ app.set("view engine", "hbs"); //sets HBS as the template engine
 hbs.registerPartials(__dirname + "/views/partials"); // config partials
 
 
+// 
+// connect to DB
+// 
+mongoose
+    .connect('mongodb://localhost/ironic-pizzas-db')
+    .then(x => {
+        console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+    })
+    .catch(err => console.error('Error connecting to mongo', err));
+
+    
+
+//
+// Routes
+//
+
+
 app.get("/", (req, res, next) => {
     console.log("this is the homepage");    
     // res.send("hello world");
     res.render("index");
 });
+
+
+
 
 
 app.get("/contact", (req, res, next) => {
@@ -25,52 +49,45 @@ app.get("/contact", (req, res, next) => {
 });
 
 
-app.get("/pizzas/margherita", (req, res, next) => {
-    //res.send("hello")
-    //res.sendFile(__dirname + '/views/pizza-margherita.html');
 
-    // const data = {
-    //     title: "Pizza Margherita",
-    //     price: 8,
-    //     imgFile: "pizza-margherita.jpg",
-    //     ingredients: ["mozzarella", "tomato sauce", "basilicum"]
-    // }
 
-    Pizza.findOne({title: "pizza margaritta"})
-        .then((pizzaFromDB) => {
-            res.render("pizza-page", pizzaFromDB);
-        }).catch((err) => {
-            
-        });
+app.get("/search", (req, res, next) => {
 
+    //const maxPrice = req.query.maxPrice;
+    const {maxPrice} = req.query;
+
+    Pizza.find({price: {$lte: maxPrice} })
+    //.sort({price: -1})
+    .then((pizzasFromDB) => {
+        res.render("search", {pizzasArr: pizzasFromDB})
+    })
+    .catch((err) => {
+        console.log("Error getting pizzas from DB", err)
+    });
+});
+
+
+
+app.get("/pizzas/:pizzaTitle", (req, res, next) => {
+    const titleOfPizza = req.params.pizzaTitle;
     
+    Pizza.findOne({title: titleOfPizza})
+    .then((pizzaFromDB) => {
+
+        if(pizzaFromDB === null) {
+            res.send("sorry, no pizza with that name")
+        } else {
+            res.render("pizza-page", pizzaFromDB);
+        }
+    }).catch((err) => {
+        console.log("Error getting pizza details from DB", err)
+    });
+
+})
 
 
-});
 
 
-app.get("/pizzas/carbonara", (req, res, next) => {
-
-    const data = {
-        title: "Pizza Carbonara",
-        price: 10,
-        imgFile: "pizza-carbonara.jpg"
-    }
-
-    res.render("pizza-page", data);
-});
-
-
-app.get("/pizzas/funghi", (req, res, next) => {
-
-    const data = {
-        title: "Pizza Funghi",
-        imgFile: "pizza-funghi.jpg",
-        ingredients: ["funghi", "mozzarella", "tomato", "basilicum"]
-    }
-
-    res.render("pizza-page", data);
-});
 
 
 
